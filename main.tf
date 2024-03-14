0e747fe3dcef1db6d7c79db7f57d1982088514c7
Nishad <nishad22b@gmail.com>
2024-03-14 05:42:44 +0530

    Nishad

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "nis-bucket" {
   name                     = "nis-hackathon-bucket"
+  project                  = "nisterraform"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
